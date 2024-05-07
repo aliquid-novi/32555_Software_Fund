c62e2cb800058a5e21bf43e3d4703ab4d5da90fa
Mark Paje <markpaje352@gmail.com>
2024-05-07 19:49:52 +1000

    test1

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,1 +1,27 @@
 # University Case Study Class Test File
+
+# University System
+# The university menu system should enable users to choose to go the Admin menu or Student Menu
+# (A) Admin
+# (S) Student
+# (X) exit
+class Student():
+
+class Admin():
+
+class Subject():
+
+class Database():
+
+class UniversitySys():
+
+    def __init__(self):
+        
+        user_input = input("University System: (A)dmin, (S)tudent, or X : ")
+
+        if user_input.upper() == 'A':
+
+    # def AdminSys(self):  ? 
+            
+
+    # def StudentSys(self): ?
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 32555_Software_Fund
 
 Assessment work for Assignment Case Study (University Case Study)
+
+test123
