88571e65ef0792d43f80a17a92614b531e967cf1
Mark Paje <markpaje352@gmail.com>
2024-05-10 16:52:02 +1000

    coded database class, beginning student course system

---
+++ Student Course System.py
@@ -0,0 +1,11 @@
+# Student Course System Criteria # 
+
+# Enroll Student can enroll in a subject – 4 maximum
+# Tracking Subject enrolment is tracked 
+# Remove a subject Remove by ID 
+# Show subjects Subject listing 
+# Change password Student can change their password 
+# Read/Write to file Student and subject data are read/written from/to file 
+# Error Handling Exceptions, errors, logical scenarios are handled 
+# Matching I/O I/O wording, coloring, indentation matches the sample 
+
--- database.py
+++ database.py
@@ -1,5 +1,16 @@
-
-
-
-class base_model():
+class DataBase():
+    
+    def write(self, txt):
+        fileHandler = open("students.data.txt", "a")
+        fileHandler.write(txt)
+        fileHandler.write("\n")
+        fileHandler.close()
+        
+    def read(self):
+        fileHandler = open("students.data.txt", "r")
+        content = fileHandler.read()
+        print(content)
+        content_list = fileHandler.readlines()
+        print(content_list)
+        fileHandler.close()
     
